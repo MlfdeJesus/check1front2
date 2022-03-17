a4713f27b68998fdca88183f48411e5fcbbf04fd
MlfdeJesus <MLFDEJESUS@GMAIL.COM>
2022-03-17 07:24:03 -0300

    Marcos Fonseca -feito parte 1.1

--- app.js
+++ app.js
@@ -3,9 +3,15 @@
     var btnMenu = document.querySelector(".l-sidebar__btn");
     var menu = document.querySelector(".l-sidebar");
     var documento = document.body;
+   
 
     btnMenu.addEventListener("click", function () {
         menu.classList.add("l-sidebar--open");
     });
+
+    menu.addEventListener("click", function () {
+        menu.classList.remove("l-sidebar--open");
+    });
+    
 
 })();
